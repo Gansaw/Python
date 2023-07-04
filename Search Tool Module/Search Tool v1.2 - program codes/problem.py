a5e5d5e127684b2975710be70eda0c288a6205ce
Gansaw <pcgame333@naver.com>
2023-07-04 18:02:10 +0900

    AI Programming수업

---
+++ Search Tool Module/Search Tool v1.2 - program codes/problem.py
@@ -0,0 +1,21 @@
+import math
+import random
+
+
+# interface
+class Problem:
+    def __init__(self):
+        
+
+
+class Numeric(Problem):
+    def __init__(self):
+        Problem.__init__(self)
+    
+
+
+class Tsp(Problem):
+    def __init__(self):
+        Problem.__init__(self)
+
+    
